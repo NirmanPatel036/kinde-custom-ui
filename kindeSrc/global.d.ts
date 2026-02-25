6752a007844beca5b967ce1b528c3e75dcfb59c2
Nirman Patel <144795287+NirmanPatel036@users.noreply.github.com>
2026-02-26 01:09:38 +0530

    delete kindeSrc directory

--- kindeSrc/global.d.ts
+++
@@ -1,6 +0,0 @@
-// Custom type declarations for modules not covered by @types/react-dom@18
-// react-dom/server.browser is a browser-optimised subpath export of react-dom
-// that @types/react-dom@18 does not include a declaration for.
-declare module "react-dom/server.browser" {
-    export { renderToString, renderToStaticMarkup } from "react-dom/server";
-}
